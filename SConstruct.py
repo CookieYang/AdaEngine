c79bc3d7f1d71614333506c6a11d691db23066fc
DESKTOP-ABBNTO3\yangtao <1359870821@qq.com>
2019-04-21 22:40:58 +0800

    init2

--- SConstruct.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import os
-
-env = Environment()
-Export('env')
-SConscript("main/SCsub")
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <gl/GL.h>
+int main() {
+	GLfloat s;
+    printf("Hello, World!\n");
+	return 0;
+}
