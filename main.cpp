99b28e957caaca22e1818bff32f4a452d32f586a
DESKTOP-ABBNTO3\yangtao <1359870821@qq.com>
2019-04-22 00:29:30 +0800

    init gl env

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <stdio.h>
-#include <gl/GL.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+
 int main() {
-	GLfloat s;
-    printf("Hello, World!\n");
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1280, 720, "Ada Engine", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("glad load failed");
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
